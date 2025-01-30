5379deaeb5e7ac107a9769b5fa2089b92ecf05ea
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2025-01-30 15:11:12 +0000

    feat: modularize SEO and formBuilder plugins into separate files

--- src/cms/plugins/index.ts
+++ src/cms/plugins/index.ts
@@ -1,64 +1,20 @@
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
-import { formBuilderPlugin } from '@payloadcms/plugin-form-builder'
-
-import { seoPlugin } from '@payloadcms/plugin-seo'
 import { searchPlugin } from '@payloadcms/plugin-search'
 
 import { Plugin } from 'payload'
-
-import { GenerateTitle, GenerateURL } from '@payloadcms/plugin-seo/types'
-import { FixedToolbarFeature, HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
 import { searchFields } from '@/cms/search/fieldOverrides'
 import { beforeSyncWithSearch } from '@/cms/search/beforeSync'
 
-import { Page, Post } from '@/cms/payload-types'
-import { getServerSideURL } from '@/utilities/getURL'
 import { redirects } from './redirects'
 import { nestedDocs } from './nestedDocs'
-
-const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
-  return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
-}
-
-const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
-  const url = getServerSideURL()
-
-  return doc?.slug ? `${url}/${doc.slug}` : url
-}
+import { seo } from './seo'
+import { formBuilder } from './formBuilder'
 
 export const plugins: Plugin[] = [
   redirects,
   nestedDocs,
-  seoPlugin({
-    generateTitle,
-    generateURL,
-  }),
-  formBuilderPlugin({
-    fields: {
-      payment: false,
-    },
-    formOverrides: {
-      fields: ({ defaultFields }) => {
-        return defaultFields.map((field) => {
-          if ('name' in field && field.name === 'confirmationMessage') {
-            return {
-              ...field,
-              editor: lexicalEditor({
-                features: ({ rootFeatures }) => {
-                  return [
-                    ...rootFeatures,
-                    FixedToolbarFeature(),
-                    HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4'] }),
-                  ]
-                },
-              }),
-            }
-          }
-          return field
-        })
-      },
-    },
-  }),
+  seo,
+  formBuilder,
   searchPlugin({
     collections: ['posts'],
     beforeSync: beforeSyncWithSearch,
