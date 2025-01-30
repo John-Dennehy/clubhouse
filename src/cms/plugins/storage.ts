7b47d31e331677e3ef2d2ebfb6018dd019dce102
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2025-01-30 17:05:28 +0000

    feat: enhance storage plugin configuration with additional options

--- src/cms/plugins/storage.ts
+++ src/cms/plugins/storage.ts
@@ -5,12 +5,16 @@ export const storage = s3Storage({
     media: true,
   },
   bucket: process.env.S3_BUCKET as string,
+  disableLocalStorage: true,
   config: {
     credentials: {
       accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
     },
     region: process.env.S3_REGION,
+    endpoint: process.env.S3_ENDPOINT,
+    forcePathStyle: true,
+
     // ... Other S3 configuration
   },
 })
