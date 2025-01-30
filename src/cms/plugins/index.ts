8e9ebd73c0d96e572fa7f76438cdc02519504e27
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2025-01-30 15:20:27 +0000

    feat: add storage plugin and reorganize plugin imports

--- src/cms/plugins/index.ts
+++ src/cms/plugins/index.ts
@@ -2,17 +2,19 @@ import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { Plugin } from 'payload'
 
 // local imports
-import { redirects } from './redirects'
-import { nestedDocs } from './nestedDocs'
-import { seo } from './seo'
 import { formBuilder } from './formBuilder'
+import { nestedDocs } from './nestedDocs'
+import { redirects } from './redirects'
 import { search } from './search'
+import { seo } from './seo'
+import { storage } from './storage'
 
 export const plugins: Plugin[] = [
-  redirects,
-  nestedDocs,
-  seo,
   formBuilder,
+  nestedDocs,
+  redirects,
   search,
+  seo,
+  storage,
   payloadCloudPlugin(),
 ]
---
+++ src/cms/plugins/storage.ts
@@ -0,0 +1,16 @@
+import { s3Storage } from '@payloadcms/storage-s3'
+
+export const storage = s3Storage({
+  collections: {
+    media: true,
+  },
+  bucket: process.env.S3_BUCKET as string,
+  config: {
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
+    },
+    region: process.env.S3_REGION,
+    // ... Other S3 configuration
+  },
+})
